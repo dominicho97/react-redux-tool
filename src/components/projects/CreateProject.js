9db535a5faf2e29f33020594cc5eebab85095eb9
dominicho97 <dominic.ho97@gmail.com>
2019-04-17 16:26:37 +0200

    added createproject code

---
+++ src/components/projects/CreateProject.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+export class CreateProject extends Component {
+  state = {
+    title: "",
+    content: "",
+    firstName: "",
+    lastName: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="content">Content</label>
+            <textarea
+              className="materialize-textarea"
+              id="content"
+              onChange={this.handleChange}
+            />
+            Project Content
+          </div>
+          <div className="input-field">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" onChange={this.handleChange} />
+          </div>
+          <div className="inputfield">
+            <button className="btn  pink lighten-1 z-depth-0">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateProject;
