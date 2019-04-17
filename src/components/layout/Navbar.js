baa2965ae30ae2f571daadde31f67e3863fa4df2
dominicho97 <dominic.ho97@gmail.com>
2019-04-17 09:28:38 +0200

    .

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <link to="/" className="brand-logo">
+          MarioPlan
+        </link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
