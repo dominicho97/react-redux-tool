cd3bd39055bf4ccf1ac4bc857718817b249a806f
dominicho97 <dominic.ho97@gmail.com>
2019-04-17 16:31:54 +0200

    added create project

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -23,28 +23,20 @@ export class CreateProject extends Component {
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <h5 className="grey-text text-darken-3">Create new project</h5>
           <div className="input-field">
             <label htmlFor="title">Title</label>
             <input type="text" id="title" onChange={this.handleChange} />
           </div>
           <div className="input-field">
-            <label htmlFor="content">Content</label>
+            <label htmlFor="content"> Project Content</label>
             <textarea
               className="materialize-textarea"
               id="content"
               onChange={this.handleChange}
             />
-            Project Content
           </div>
-          <div className="input-field">
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" onChange={this.handleChange} />
-          </div>
+
           <div className="inputfield">
             <button className="btn  pink lighten-1 z-depth-0">Sign Up</button>
           </div>
