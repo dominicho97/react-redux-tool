01ba0cd854627dcd2cb73efb623e19ac8be98a1c
dominicho97 <dominic.ho97@gmail.com>
2019-04-17 09:32:48 +0200

    added link

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
-        <link to="/" className="brand-logo">
+        <Link to="/" className="brand-logo">
           MarioPlan
-        </link>
+        </Link>
       </div>
     </nav>
   );
